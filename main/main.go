58252bea31c31f217df649afff787001cb073fce
Daqlet <81481220+Daqlet@users.noreply.github.com>
2021-06-05 13:55:48 +0600

    Update main.go

--- main/main.go
+++ main/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	v1 := "123"
+	v2 := 123
+	v3 := "Why I can`t get repos:(\n"
+	v4 := "abc"
+	fmt.Print(v1, v2, v3, v4)
+	fmt.Println()
+	fmt.Println(v1, v2, v3, v4)
+	fmt.Printf("%s %d %s %s\n", v1, v2, v3, v4)
 }
